5e8577402aef946ec392dc6864c017f58a14e816
Nithin S Varrier <nithin486@hotmail.com>
2024-02-08 22:18:05 +0530

    feat: add small text on top editing fields

--- lib/features/zen/zenmode_page.dart
+++ lib/features/zen/zenmode_page.dart
@@ -1,13 +1,20 @@
+import 'package:animated_visibility/animated_visibility.dart';
 import 'package:backfeed/common/ui/blurred_modal_bottom_sheet.dart'
     deferred as modal_bottom_sheet;
 import 'package:backfeed/features/zen/widgets/bottom_sheet.dart'
     deferred as emotion_bottom_sheet;
 import 'package:flutter/material.dart';
 import 'package:flutter_swipe_detector/flutter_swipe_detector.dart';
+import 'package:sprung/sprung.dart';
 
-class ZenModePage extends StatelessWidget {
+class ZenModePage extends StatefulWidget {
   const ZenModePage({super.key});
 
+  @override
+  State<ZenModePage> createState() => _ZenModePageState();
+}
+
+class _ZenModePageState extends State<ZenModePage> {
   Future<void> _handleSwipeUp(Offset? offset, BuildContext context) async {
     if (offset == null) return;
     await modal_bottom_sheet.loadLibrary();
@@ -23,20 +30,36 @@ class ZenModePage extends StatelessWidget {
     );
   }
 
+  String _textContent = "";
+
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const SizedBox(height: 35),
-        const Expanded(
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            "Thoughts are saved automatically.",
+            style: TextStyle(
+              fontSize: 16,
+              color: Color(0xff808080),
+            ),
+          ),
+        ),
+        Expanded(
           child: TextField(
+            onChanged: (value) => setState(() {
+              _textContent = value;
+            }),
             autofocus: true,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               hintText: "Start typing...",
-              contentPadding: EdgeInsets.all(16),
+              contentPadding: EdgeInsets.symmetric(horizontal: 16),
               border: InputBorder.none,
             ),
-            style: TextStyle(
+            style: const TextStyle(
               fontSize: 21,
             ),
             maxLines: null,
@@ -44,21 +67,28 @@ class ZenModePage extends StatelessWidget {
             expands: true,
           ),
         ),
-        SwipeDetector(
-          behavior: HitTestBehavior.translucent,
-          onSwipeUp: (Offset? offset) => _handleSwipeUp(offset, context),
-          child: Container(
-            // color: Color(0xFF545454),
-            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 0),
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Swipe to save",
-                  style: TextStyle(
-                      color: Color(0xFF545454), fontSize: 14, height: 1.64),
-                )
-              ],
+        AnimatedVisibility(
+          enter: slideInVertically(
+              initialOffsetY: 1, curve: Sprung.criticallyDamped),
+          exit: slideOutVertically(
+              targetOffsetY: 1, curve: Curves.fastEaseInToSlowEaseOut),
+          visible: _textContent.split(" ").length >= 3,
+          child: SwipeDetector(
+            behavior: HitTestBehavior.translucent,
+            onSwipeUp: (Offset? offset) => _handleSwipeUp(offset, context),
+            child: Container(
+              // color: Color(0xFF545454),
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 0),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Swipe to save",
+                    style: TextStyle(
+                        color: Color(0xFF545454), fontSize: 14, height: 1.64),
+                  )
+                ],
+              ),
             ),
           ),
         )
